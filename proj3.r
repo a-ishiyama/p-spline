722c5282d09ccb8c5b642fa625841981ee531685
a-ishiyama <akira.j.ishiyama@gmail.com>
2022-11-03 23:39:15 +0000

    updating

--- proj3.r
+++ proj3.r
@@ -69,6 +69,9 @@ demo <-pspline(x,y)
 #Q2
 
 print.pspline<- function(m){
+  #   This function defines the method function print() for the class 'pspline'.
+  #   
+  
   cat('Order', m$b.order,'p-spline with order',m$p.order, "penalty", "\n")
   cat("Effective degrees of freedom:",m$edf,"   Coefficients:",
       m$number.of.coefficients, "\n")
@@ -82,21 +85,63 @@ print.pspline<- function(m){
 
 #Q3
 
-predict.pspline <- function(m,x,se=TRUE){
-  x<-x[order(x)] #Order the new x data in an ascending order.
-  Xp<- splines::splineDesign(knots=m$knots,x,ord=m$b.order+1,outer.ok=TRUE)
+predict.pspline <- function(m, x, se = TRUE){
+  #   The following defines a method function predict() for the class 'pspline'.
+  #   It takes the argument se = TRUE/FALSE depending on the user if he/she 
+  # wants to obtain the standard error alongside the predicted value.
+  #   The prediction is made using the estimated coefficients from m, belonging 
+  # to the class 'pspline', and a new set of x values.
+  #   The new x data vector is first transformed into an Xp matrix with the
+  # number of observation of x as its number of row, and the number of column as
+  # the same as the number of coefficients. This Xp matrix is computed the same
+  # way as when the estimated coefficients are first estimated for pspline.
+  # knots, B-spline and penalty orders are the same as for the training data.
+  #    The derivation of covariance matrix for the construction of standard
+  # errors is efficiently computed using Cholesky decomposition.
+  
+  x <- x[order(x)] #Order the new x data in an ascending order.
+  
+  Xp <- splines::splineDesign(knots = m$knots, x, ord = m$b.order+1,
+                              outer.ok = TRUE)
+  # This creates the X matrix using x vector essentially the same method as
+  # used when we first estimated the coefficients etc, in pspline.
+  
   prediction <- Xp%*%m$coef
   # Save fitted values obtained by applying the estimated coefficients from 
   # the training data and the new Xp observation matrix.
   
-  if (se==FALSE){
-    list(prediction=prediction)
+  if (se == FALSE){
+    list(prediction = prediction) 
+    # If se argument in the method function is set as FALSE, then s.e. is not 
+    # required by the user; hence, returning only the list containing the 
+    # predictions.
+    
   }else{
-    sigma_matrix <- diag(m$sig2,m$number.of.coefficients)
-    cholXp <- chol(crossprod(Xp)+(m$lsp*crossprod(m$D)))
+    # The following lines are built to estimate the standard error of the
+    # prediction when the user state the argument for se as TRUE, which is the
+    # default.
+    
+    sigma_matrix <- diag(m$sig2, m$number.of.coefficients)
+    # Create a variance matrix that has the variance of the estimation residuals
+    # on the diagonal line. The number of column should be the number of
+    # coefficients that have been estimated as we are interested in the 
+    # covariance of the coefficients.
+    
+    cholXp <- chol(crossprod(Xp) + (m$lsp * crossprod(m$D)))
+    # The covariance matrix has the formula (X^T*X+lambda*D^T*D)^{-1}*sigma^2.
+    # It can be inefficient to compute the product before sigma^2. Hence,
+    # first Cholesky decompose (X^T*X+lambda*D^T*D).
     covariance_matrix <-backsolve(cholXp, forwardsolve(t(cholXp),sigma_matrix))
-    se <- rowSums(Xp*(Xp%*%covariance_matrix))^0.5
-    list(fit=prediction,se=se)
+    # Then, forwardsolve the transpose of the product from the Cholesky
+    # decomposition with sigma^2. This will provide the product to backsolve
+    # with the product of Cholesky decomposition.
+    
+    se <- rowSums(Xp * (Xp %*% covariance_matrix)) ^0.5
+    # Save the standard error. This method is more efficient in computation
+    # compared to computing (Xp%*%Covariance_matrix%*%t(XP)).
+    
+    list(fit = prediction, se = se) 
+    # Return the list containing the fitted value and the standard error.
   }
 }
 
@@ -105,47 +150,66 @@ predict.pspline <- function(m,x,se=TRUE){
 
 #Q4
 plot.pspline <- function(m){
-  #
+  #   The following creates a method function that produces three plots for 
+  # the results to be obtained from the class 'pspline'.
+  #   The first plot produces a scatter plot of the original y data against 
+  # x data, and on top of it draws an estimated smooth function line. 
+  # This plot also has dotted blue line that represents the 95% confidence 
+  # interval for the smooth line.
+  #   The second plot produces a residual plot, plotting residuals against the 
+  # fitted value from pspline. It is to check whether the model satisfies the
+  # assumption that the residual has mean zero with no obvious distribution
+  # pattern.
+  #   The last plot produces a Q-Q plot of the residuals. It enable the user to 
+  # visibly check whether the model satisfies the residual normality 
+  # assumption.
   
-  x_4 <- seq(m$x[1],m$x[length(m$x)], length=length(m$x))
   
-  upper <- lower <-c()
-  # Create vectors containing observation numbers of 0s to later save the upper
-  # and lower bound of the confidence interval for the fitted value.
-
+  upper <- lower <- c()
+  # Create empty vectors to later save the upper and lower bound of the 
+  # confidence interval for the fitted value.
+  
+  x_4 <- seq(m$x[1],m$x[length(m$x)], by = 0.1)
+  # 
+  
   P <- predict.pspline(m, x_4)
-  
+  # Use the previously defined method function of predict.pspline to estimate
+  # the 
 
-  upper <- P$fit + 1.96* P$se
+  upper <- P$fit + 1.96 * P$se
   # Save the upper confidence interval for the estimated fitted value.
-  lower <- P$fit - 1.96* P$se
+  lower <- P$fit - 1.96 * P$se
   # Save the lower confidence interval for the estimated fitted value.
-    
+   
   
-  plot(m$x,m$y,main="Original data with the estimated smooth function", 
-       xlab="x", ylab="y")
+  plot(m$x,m$y,main = "Original data with the estimated smooth function", 
+       xlab = "x", ylab = "y")
   # Plot a scatter plot of the original y data against x.
-  lines(x_4,P$fit, col="red")
+  lines(x_4,P$fit, col = "red")
   # Draw the estimated smooth function inside the above plot in red.
-  lines(x_4,upper, lty=3, col="blue")
+  lines(x_4,upper, lty = 3, col = "blue")
   # Draw the upper confidence interval in a blue dotted line in the above plot.
-  lines(x_4,lower, lty=3, col="blue")
+  lines(x_4,lower, lty = 3, col = "blue")
   # Draw the lower confidence interval in a blue dotted line in the above plot.
-  legend(30, -95, legend=c("estimated smooth function",
-                           "95% credible intervals"),col=c("red", "blue"), 
-         lty=1:2)
+  legend(30, -95, legend = c("estimated smooth function",
+                           "95% credible intervals"),col = c("red", "blue"), 
+         lty = 1:2)
   # Set legend to increase the visibility.
   
-  plot(m$fitted, m$residual, main="Model residuals against fitted values", 
-       xlab="fitted value", ylab="model residuals")
+  
+  plot(m$fitted, m$residual, main = "Model residuals against fitted values", 
+       xlab ="fitted value", ylab = "model residuals")
   # Plot the residuals against the estimated fitted value from pspline.
   abline(h=0) 
-  # Set a line at y=0 to visibly assess the assumption of residual normality.
+  # Set a line at y=0 to visibly assess the assumption of residual having a mean
+  # value of zero, and randomly scattered.
+  
   
   qqnorm(m$residual) # Create the Q-Q plot using the residual from pspline.
   qqline(m$residual) # Set a reference line in the above Q-Q plot.
   
-  plot_list<-list(cbind(ll=lower,ul=upper,x=x_4))
+  
+  plot_list <- list(ll = lower, ul = upper, x = x_4)
   invisible(plot_list)
   # Invisibly return a list containing the lower and upper confidence interval,
   # and the x observations.
